94c6a09b14b3a27fe90f5683031129f3a7ca806b
omarsagoo <om.sagoo@gmail.com>
2021-02-17 16:00:55 -0800

    setup post model, db, controllers.

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require("express")
 const app = express()
 
+
 const exphb = require("express-handlebars")
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Set db
+require('./data/reddit-db');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+require('./controllers/posts.js')(app);
 
 app.engine("handlebars", exphb({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 app.get("/", (req, res) => {
     res.render("home")
+})
+
+app.get("/posts/new", (req, res) => {
+    res.render("post-new")
 })
 
 // Start Server
